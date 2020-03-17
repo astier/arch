9183598764cf8ded49ecca22e085520842069dfe
aleks <aleks.stier@icloud.com>
2020-03-17 03:34:25 +0100

    Merge packages and projects into programs section

--- desktop.sh
+++ desktop.sh
@@ -1,23 +1,21 @@
 #!/usr/bin/env sh
 
 # INTERNET
-sudo cp ~/projects/dotfiles/dotfiles/iwd.conf /etc/iwd/main.conf
+cd ~/projects || exit
+sudo cp dotfiles/dotfiles/iwd.conf /etc/iwd/main.conf
 sudo systemctl enable --now iwd.service systemd-resolved.service
 sudo ln -fs /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 
-# PACKAGES
-sudo pacman -S alsa-utils arc-gtk-theme dash fakeroot firefox fzf gcc light make man-db noto-fonts-cjk python-pynvim sx sxhkd tmux ttf-dejavu xorg-server xorg-xsetroot xsel yarn
+# PROGRAMS
+sudo pacman -S alsa-utils arc-gtk-theme dash fakeroot firefox fzf gcc light make man-db python-pynvim sx sxhkd tmux ttf-dejavu xorg-server xorg-xsetroot xsel yarn
 git clone https://aur.archlinux.org/yay
-cd yay && makepkg -is
-yay -S dropbox
-
-# PROJECTS
-cd ~/projects || exit
 git clone https://github.com/astier/dwm
 git clone https://github.com/astier/st
 cd dwm && sudo make install clean
 cd ../scripts && sh setup.sh
 cd ../st && sudo make install clean
+cd ../yay && makepkg -is
+yay -S dropbox xbanish
 
 # CONFIG
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
