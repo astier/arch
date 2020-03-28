ce1287ce6dbfddc853ea0911680e13b26064393c
aleks <aleks.stier@icloud.com>
2020-03-28 12:15:26 +0100

    Replace yay with yay-bin

--- desktop.sh
+++ desktop.sh
@@ -8,13 +8,13 @@ sudo ln -fs /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 
 # PROGRAMS
 sudo pacman -S alsa-utils arc-gtk-theme dash fakeroot firefox fzf gcc light make man-db python-pynvim rclone sx sxhkd tmux ttf-dejavu xorg-server xorg-xset xorg-xsetroot xsel
-git clone https://aur.archlinux.org/yay
+git clone https://aur.archlinux.org/yay-bin
 git clone https://github.com/astier/dwm
 git clone https://github.com/astier/st
 cd dwm && sudo make install clean
 cd ../scripts && sh setup.sh
 cd ../st && sudo make install clean
-cd ../yay && makepkg -is
+cd ../yay-bin && makepkg -is
 yay -S xbanish
 
 # CONFIG
@@ -27,7 +27,7 @@ sudo systemctl enable fstrim.timer iptables.service systemd-timesyncd.service
 sudo usermod -aG video "$USER" # fix broken light-package
 
 # CLEAN
-cd && rm -fr .bash_logout .cache/* projects/yay
-sudo pacman -Rns efibootmgr go
+cd && rm -fr .bash_logout .cache/* projects/yay-bin
+sudo pacman -Rns efibootmgr
 sudo pacman -Sc
 sudo reboot
